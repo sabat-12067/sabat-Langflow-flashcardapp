9011572e90c067740a0519a1a0b2f9b312054ae3
sabat-12067 <greatdev12067@gmail.com>
2024-01-25 18:52:51 -0500

    added more stuff on the landing page

--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -1,18 +1,35 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Auth } from "@supabase/auth-ui-react";
 import { supabase } from "../libs/supabase";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
+import {useSelector } from "react-redux";
 
 interface AuthFormProps {}
 const AuthForm: FC<AuthFormProps> = ({}) => {
+
+  const navigate = useNavigate();
+  const user = useSelector((state: any) => state.auth.user)
+
+
+  useEffect(() => {
+     supabase.auth.onAuthStateChange((event, session) => {
+      if (session) {
+        navigate('/cards');
+      }
+    });
+  }, [navigate]);
+
+
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="outline" className="text-black">Get Started</Button>
       </DialogTrigger>
-      <DialogContent className="bg-[#020617]">
+      <DialogContent className="bg-[#020617] p-8">
         <Auth
           supabaseClient={supabase}
           providers={["google"]}
--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,6 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
-import { persistStore, persistReducer } from 'redux-persist'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 import storage from 'redux-persist/lib/storage' 
 
 
@@ -16,7 +25,13 @@ const rootReducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 })
 
 export const persistor = persistStore(store);
