579f51991f1c50b2dac162787641c980ba0cff9f
sabat-12067 <greatdev12067@gmail.com>
2024-02-10 15:04:43 -0500

    changed the user_id in my django MVC files to reflect a string

--- backend/flashcards/models.py
+++ backend/flashcards/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class StudyClass(models.Model): 
-    user_id = models.CharField(None)
+    user_id = models.CharField(max_length=96)
     name = models.CharField(max_length = 50)
     description = models.TextField(blank=True)
 
--- backend/flashcards/urls.py
+++ backend/flashcards/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 
 urlpatterns = [
     path('study-classes/', StudyClassView.as_view(), name='study-class'),
-    path('study-classes/<int:user_id>/', StudyClassView.as_view(), name='study-class-update'),
+    path('study-classes/<str:user_id>/', StudyClassView.as_view(), name='study-class-update'),
     path('study-classes/<int:study_class_id>/flashcard-sets/', FlashCardSetView.as_view(), name='flashcard-set-list'),
     path('study-classes/<int:study_class_id>/flashcard-sets/<int:flashcard_set_id>/', FlashCardSetView.as_view(), name='flashcard-set-update'),
     path('study-classes/<int:flashcard_set_id>/flashcards/', FlashCardView.as_view(), name='flashcards'),
