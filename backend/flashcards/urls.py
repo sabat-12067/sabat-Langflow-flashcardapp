f2d1376ef9f6f2de4d658de1fe4fd443564c7041
sabat-12067 <greatdev12067@gmail.com>
2024-04-24 14:12:55 -0400

    added modal for making a new card, along with redux services for fetching

--- backend/flashcards/urls.py
+++ backend/flashcards/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('study-classes/<int:study_class_id>/flashcard-sets/', FlashCardSetView.as_view(), name='flashcard-set-list'),
     path('study-classes/<int:study_class_id>/flashcard-sets/<int:flashcard_set_id>/', FlashCardSetView.as_view(), name='flashcard-set-update'),
     path('study-classes/<int:flashcard_set_id>/flashcards/', FlashCardView.as_view(), name='flashcards'),
-    #How to GET a classrooms flashcards sets cards
+    #How to GET, POST a classrooms flashcards sets cards
     path('flashcards/<int:flashcard_set_id>/', FlashCardView.as_view(), name='flashcard-detail'),
     #How to POST cards for a classrooms flashcards set
-#   /path('flashcards/<int:flashcard_id>/', FlashCardView.as_view(), name='flashcard-detail'),
+#   path('flashcards/<int:flashcard_id>/', FlashCardView.as_view(), name='flashcard-detail'),
 ]
