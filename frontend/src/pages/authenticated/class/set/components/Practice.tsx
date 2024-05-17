d11634a49cc0835fb21302dd179ec2ada8464378
sabat-12067 <greatdev12067@gmail.com>
2024-05-17 15:35:12 -0400

    Fixing UI styles for practice FlashCard component, added sound icon

--- frontend/src/pages/authenticated/class/set/components/Practice.tsx
+++ frontend/src/pages/authenticated/class/set/components/Practice.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import {
   Drawer,
   DrawerContent,
@@ -9,28 +15,23 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Card } from "@/types";
-import { HoverCardContent } from "@radix-ui/react-hover-card";
 import clsx from "clsx";
 import { FC, useState } from "react";
+import { AiOutlineSound } from "react-icons/ai";
 import { PiCards } from "react-icons/pi";
 import { useSelector } from "react-redux";
-interface PracticeProps {
-    cards: Card[]
-}
-const Practice: FC<PracticeProps> = ({
-cards
-}) => {
-  const isDarkMode = useSelector((content: any) => content.theme.isDarkMode);
-  const [switchSides, setSwitchSides] = useState(false)
-    
-  return (
 
+interface PracticeProps {
+  cards: Card[];
+}
+const Practice: FC<PracticeProps> = ({ cards }) => {
+  const isDarkMode = useSelector((content: any) => content.theme.isDarkMode);
+  const [switchSides, setSwitchSides] = useState(false);
+
+  return (
     <Drawer>
       <DrawerTrigger>
-        <Button
-         className="flex gap-1 mb-8" 
-         variant={"outline"}
-         >
+        <Button className="flex gap-1 mb-8" variant={"outline"}>
           <span className="hidden md:block">Practice</span>
           <PiCards size={17} />
         </Button>
@@ -41,27 +42,31 @@ cards
           isDarkMode ? "" : "bg-black text-white"
         )}
       >
-       <div className="text-center mx-auto py-[130px]">
-       <Carousel className="w-full max-w-xs">
-      <CarouselContent className="">
-        {
-            cards?.map((card, i) => {
+        <div className="text-center mx-auto py-[130px]">
+          <Carousel className="w-full max-w-xs">
+            <CarouselContent className="">
+              {cards?.map((card, i) => {
                 return (
-                    <CarouselItem
-                     key={i}
-                     className="h-[500px] w-[400px] bg-[#090c19] rounded-lg text-center py-[212px] text-5xl"
-                     onClick={() => setSwitchSides(!switchSides)}
-                     >
-                        {switchSides ?  card.front : card.back}
-                    </CarouselItem>
-                )
-            })
-        }
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-       </div>
+                  <CarouselItem
+                    key={i}
+                    className={clsx(
+                      "h-[500px] w-[400px] rounded-lg text-center py-[212px] text-5xl",
+                      isDarkMode ? "bg-slate-100" : "bg-[#090c19]"
+                    )}
+                    onClick={() => setSwitchSides(!switchSides)}
+                  >
+                    {switchSides ? card.front : card.back}
+                    <AiOutlineSound className="fixed bottom-2 left-6" color="black" size={30} />
+
+                  </CarouselItem>
+                );
+              })}
+
+            </CarouselContent>
+            <CarouselPrevious className="text-black" />
+            <CarouselNext className="text-black" />
+          </Carousel>
+        </div>
       </DrawerContent>
     </Drawer>
   );
