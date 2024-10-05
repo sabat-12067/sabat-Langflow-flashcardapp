fcc0cb501df93557a6b2274c4f03dd97155de6b4
sabat-12067 <greatdev12067@gmail.com>
2024-10-05 14:05:12 -0400

    Added db_index=True for django queries to improve performance

--- backend/flashcards/models.py
+++ backend/flashcards/models.py
@@ -2,26 +2,26 @@ from django.db import models
 from django.utils.timezone import now
 
 class StudyClass(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    user_id_or_study_class_id = models.CharField(max_length=96)
-    name = models.CharField(max_length=50, unique=True)
-    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    user_id_or_study_class_id = models.CharField(max_length=96, db_index=True)
+    name = models.CharField(max_length=50, unique=True,db_index=True)
+    description = models.TextField(blank=True, db_index=True)
 
     def __str__(self):
         return self.name
 
 class FlashCardSet(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    study_class = models.ForeignKey(StudyClass, related_name='sets', on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
-    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    study_class = models.ForeignKey(StudyClass, related_name='sets', on_delete=models.CASCADE, db_index=True)
+    name = models.CharField(max_length=20, db_index=True)
+    description = models.TextField(blank=True, db_index=True)
 
     def __str__(self):
         return self.name
 
 class FlashCards(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    flashcard_set = models.ForeignKey(FlashCardSet, related_name='flashcards', on_delete=models.CASCADE)
+    flashcard_set = models.ForeignKey(FlashCardSet, related_name='flashcards', on_delete=models.CASCADE, db_index=True)
     front = models.TextField()
     back = models.TextField()
 
