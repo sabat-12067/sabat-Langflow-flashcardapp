dcfcfab6c3d1dc849277e80ec45a80264bb0c1f1
sabat-12067 <greatdev12067@gmail.com>
2024-10-13 12:40:15 -0400

    addding Elastic search models

--- backend/flashcards/search_indexes.py
+++ backend/flashcards/search_indexes.py
@@ -1,3 +1,14 @@
 from haystack import indexes
 from .models import StudyClass, FlashCardSet, FlashCards
 
+#Search index for StudyClass
+class StudyClassIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharFiled(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name')
+    description = indexes.CharField(model_attr = 'description')
+
+    def get_model(self):
+      return StudyClass
+    def index_queryset(self, using=None):
+      return self.get_model().objects.all()
+        
