3a1d9b85c7cd9e1ec503300e72e516e69475c152
sabat-12067 <greatdev12067@gmail.com>
2024-10-17 16:44:01 -0400

    adding Elastic Search indexes for all three classes

--- backend/flashcards/search_indexes.py
+++ backend/flashcards/search_indexes.py
@@ -12,3 +12,31 @@ class StudyClassIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
       return self.get_model().objects.all()
         
+# class FlashCardSetIndex
+
+class FlashCardSetIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr="name")
+    description = indexes.CharField(model_attr='description')
+    study_class = indexes.CharField(model_attr='study_class_name')
+
+    def get_model(self):
+        return FlashCardSet
+    
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+# Search index for FlashCards
+class FlashCardsIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    front = indexes.CharField(model_attr='front')
+    back = indexes.CharField(model_attr='back')
+    flashcard_set = indexes.CharField(model_attr='flashcard_set_name')
+    study_set = indexes.CharField(model_attr='flashcard_set_study_class_name')
+
+    def get_model(self):
+        return FlashCards
+    
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+    
